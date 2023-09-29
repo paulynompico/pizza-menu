2578ca131143343abcc7e08abd2d2f7a670a5aa2
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-09-29 11:50:54 +0200

    Use strict mode

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello!</h1>;
+  return <h1>Hello! I am a potato.</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
