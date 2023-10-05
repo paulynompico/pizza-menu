f6f444f5e6ca950619c17e1877b0bf40a6166d59
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-10-05 13:12:27 +0200

    Add container to center components

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -87,11 +87,13 @@ function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
-  const ifOpen = hour >= openHour && hour <= closeHour;
+  const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} - We are currently open!
+      <p>
+        We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+      </p>
     </footer>
   );
 }
